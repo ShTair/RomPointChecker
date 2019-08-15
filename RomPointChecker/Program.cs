3883a45bfd48ea3bba9c4e79c56bcc38a6e7d794
ShTair <git@vsh.jp>
2019-08-15 10:35:12 +0900

    実装

--- RomPointChecker/Program.cs
+++ RomPointChecker/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using RomController;
+using ShComp;
+using System;
+using System.Drawing;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RomPointChecker
 {
@@ -10,6 +11,51 @@ namespace RomPointChecker
     {
         static void Main(string[] args)
         {
+            RunAsync().Wait();
+        }
+
+        private static async Task RunAsync()
+        {
+            while (true)
+            {
+                var p = await GetPointAsync();
+                var w = await RomWindow.FromPointAsync(p);
+
+                if (w is null) break;
+
+                var r = w.Rectangle;
+                var dx = (double)(p.X - r.X) / r.Width;
+                var dy = (double)(p.Y - r.Y) / r.Height;
+
+                Console.WriteLine($"({p.X}, {p.Y}) -> ({dx:0.0000}, {dy:0.0000})");
+            }
+
+            Console.WriteLine("停止しました。");
+            Console.ReadLine();
+        }
+
+        private static Task<Point> GetPointAsync()
+        {
+            var tcs = new TaskCompletionSource<Point>();
+            var hooker = new MouseHooker();
+
+            hooker.EventReceived += (status, point) =>
+            {
+                if (status == 514)
+                {
+                    tcs.TrySetResult(point);
+                    Application.Exit();
+                }
+            };
+
+            Task.Run(() =>
+            {
+                hooker.Start();
+                Application.Run();
+                hooker.Stop();
+            });
+
+            return tcs.Task;
         }
     }
 }
